e4de13dd94fed0426909b0bac0dbebefa8dbfece
Alexander <alexander.martinez@lapatota.cl>
2022-07-23 00:07:27 -0400

    input setters

--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -1,30 +1,54 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
-export class ErrMsgDirective implements OnInit {
+export class ErrMsgDirective implements OnInit, OnChanges {
+
+  private _color: string = 'red'
+  private _mensaje: string = 'Este campo es requerido'
 
   htmlElement: ElementRef<HTMLElement>;
-  @Input() color: string = 'red';
-  @Input() mensaje: string = 'Este campo es requerido';
+
+  // al recibir la propiedad del padre seteamos de una el color del elemento
+  @Input() set color(valor: string) {
+    this.htmlElement.nativeElement.style.color = valor;
+    this._color = valor;
+  }
+
+
+  // @Input() mensaje: string = 'Este campo es requerido';
+
+  @Input() set mensaje(valor: string) {
+    console.log(valor);
+    this.htmlElement.nativeElement.innerText = valor;
+    this._mensaje = valor
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
   }
 
-  ngOnInit(): void {
-    this.setColor();
-    this.setMensaje();
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log(changes);
   }
 
-  setColor(): void {
-    this.htmlElement.nativeElement.style.color = this.color;
+  ngOnInit(): void {
+    /*  console.log(this.color, this.mensaje);
+        this.setColor();
+        this.setMensaje(); */
+  }
+
+  setEstilo(): void {
     this.htmlElement.nativeElement.classList.add('form-text');
   }
 
-  setMensaje(): void {
-    this.htmlElement.nativeElement.innerText = this.mensaje;
-  }
+  // setColor(): void {
+  //   this.htmlElement.nativeElement.style.color = this.color;
+  // }
+
+  // setMensaje(): void {
+  //   this.htmlElement.nativeElement.innerText = this.mensaje;
+  // }
 
 }
