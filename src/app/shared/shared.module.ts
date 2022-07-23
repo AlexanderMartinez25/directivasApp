a0d8ed59d59d2de345b516c2ec3f409f10779a48
Alexander <alexander.martinez@lapatota.cl>
2022-07-22 22:39:35 -0400

    directiva personalizada - ErrorMsg

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ErrMsgDirective } from './directives/err-msg.directive';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  // es parte de este modulo
+  declarations: [
+    ErrMsgDirective
+  ],
+  // se exporta para usarse en otros modulos
+  exports: [
+    ErrMsgDirective
   ]
 })
 export class SharedModule { }
